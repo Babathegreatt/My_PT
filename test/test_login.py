74fe86429abcc12c3982fbd0f6c1e0a14dd4bfd9
Gaurav <gaurav.inamdar@hotmail.com>
2023-03-07 18:14:36 +0530

    trying pytest

---
+++ test/test_login.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+class TestLogin:
+
+    #@pytest.fixture(autouse=True)
+    def test_login(self):
+        print("login")
+
+    def print_string(self):
+        print("string")
+
